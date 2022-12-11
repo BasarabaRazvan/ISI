11c5050b8d99954eb95f2bc84b501319453f8137
BasarabaRazvan <razvan_basaraba@yahoo.ro>
2022-12-11 09:57:53 +0200

    Setup React + Afisare harta Las Vegas

---
+++ app/src/App.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useRef } from 'react'
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+
+function App() {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    new MapView({
+      map: new Map({
+        basemap: "streets"
+      }),
+      container: mapRef.current,
+      center: [-115.151507, 36.131516],
+      zoom: 11
+    });
+  }, []);
+
+  return (
+    <div ref={mapRef} style={{ height: "100vh", width: "100%"}}>
+
+    </div>
+  )
+}
+
+export default App;
